9d1588ebb19c72cda2257ab249060ef3b0595fb5
arka-bumi <muhammadzaky.works@gmail.com>
2026-02-07 20:03:01 +0700

    Update SQL_Backwood_Query.sql

--- sql/Backwood Paper_Dataset/SQL_Backwood_Query.sql
+++ sql/Backwood Paper_Dataset/SQL_Backwood_Query.sql
@@ -1,1 +1,7 @@
+/* “I know that the price/standard_qty paper varies from one order to the next.
+I would like this ratio across all of the sales made.” */
 
+SELECT
+  SUM(standard_amt_usd) / SUM(standard_qty) AS avg_price_per_standard_paper
+FROM `backwoodpaper_sql_tutorial.orders`
+WHERE standard_qty > 0
